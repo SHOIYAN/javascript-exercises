2df7ea9d618acd0b2f636cd62b25981590c673c8
SHOIYAN <shoiyan9@gmail.com>
2025-08-20 21:09:49 +0300

    feat: solve repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let content = '';
+    if(num < 0){
+        return 'ERROR';
+    }
+    else {
+        for(let i = 0; i<num; i++){
+        content = content.concat(string);
+        }
+    }
+    
+    return content;
 };
-
+repeatString('hey', 2);
 // Do not edit below this line
 module.exports = repeatString;

7698cf69fcfffa60da91928e8cd7051a127679d7
SHOIYAN <shoiyan9@gmail.com>
2025-08-20 22:43:22 +0300

    solved reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reversed = [...string].reverse().join('');
+    return reversed;
 };
 
+reverseString ('hello me');
 // Do not edit below this line
 module.exports = reverseString;

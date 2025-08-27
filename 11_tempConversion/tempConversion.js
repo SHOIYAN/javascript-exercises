1d80bea486d4bc3b1943011d45fae542cb64bfe8
SHOIYAN <shoiyan9@gmail.com>
2025-08-28 01:13:39 +0300

    feat: solve tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+  return (degCelsius = Math.round((deg - 32) * (5 / 9) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (deg) {
+  return (degFahrenheit = Math.round(((deg * 9/5 ) + 32 ) * 10) / 10);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

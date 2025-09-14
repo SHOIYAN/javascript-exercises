0124b968da5cc6943fada4bfb693798f5f7174c0
SHOIYAN <shoiyan9@gmail.com>
2025-09-14 23:49:52 +0300

    feat: solve palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-z0-9]/gi, "");
+  return string === string.split("").reverse().join("");
 };
 
 // Do not edit below this line

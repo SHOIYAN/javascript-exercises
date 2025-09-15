465c7a8dc3f81b9f020dab22e7913664cc01b8b5
SHOIYAN <shoiyan9@gmail.com>
2025-09-15 18:49:53 +0300

    feat: solve fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  index = Number(index);
+  if (index < 0) return "OOPS"; // handle negatives
+  if (index === 0 ) return 0;
+  if (index === 1) return 1;
 
+  let a = 0, b = 1;
+  for (let i = 2; i <= index; i++) {
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;

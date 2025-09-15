e7d538d08a94a0c3a05707e4b777ce60b3f131da
SHOIYAN <shoiyan9@gmail.com>
2025-09-15 22:24:18 +0300

    feat: solve findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // get greatest age
+  const age = people.reduce((acc, person) => {
+    person.yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    let age = person.yearOfDeath - person.yearOfBirth;
+    if (age > acc) {
+      acc = age;
+    }
+    return acc;
+  }, 0);
+  // get person's object matching the age
+  const obj = people.find(
+    (person) => person.yearOfDeath - person.yearOfBirth === age
+  );
+  // return the obj
+  return obj;
 };
 
 // Do not edit below this line

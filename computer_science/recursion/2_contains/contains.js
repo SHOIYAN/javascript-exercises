000345571ebe227c77b204935e33e979e5447043
SHOIYAN <shoiyan9@gmail.com>
2026-01-05 23:55:34 +0300

    feat: complete contain.js

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,15 @@
-const contains = function() {
-  
+const contains = function (obj, searchValue) {
+  for (const value of Object.values(obj)) {
+    if (Number.isNaN(searchValue) && Number.isNaN(value)) {
+      return true;
+    }
+    if (value === searchValue) return true;
+
+    if (typeof value === "object" && value !== null) {
+      if (contains(value, searchValue)) return true;
+    }
+  }
+  return false;
 };
-  
 // Do not edit below this line
 module.exports = contains;

c1c0c6a342251b513f7db9157d7115543c300c61
SHOIYAN <shoiyan9@gmail.com>
2026-01-07 20:07:41 +0300

    feat: complete totalIntergers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,18 @@
-const totalIntegers = function() {
+const totalIntegers = function (obj) {
+  let total = 0;
+
+  if (typeof obj !== 'object' || obj === null) return undefined;
+
+  for (const prop of Object.values(obj)) {
+    if (Number.isInteger(prop)) total++;
+    if (typeof prop === "object" && prop !== null) total += totalIntegers(prop); 
+  }
   
+  return total;
 };
-  
+
+totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // returns 7
+totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } }); // returns 4
+
 // Do not edit below this line
 module.exports = totalIntegers;

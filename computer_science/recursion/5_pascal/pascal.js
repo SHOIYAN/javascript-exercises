132ba3d9be9b178ca2bfc3bf524adac1af67f675
SHOIYAN <shoiyan9@gmail.com>
2026-01-07 22:28:51 +0300

    feat: finish pascal exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,16 @@
-const pascal = function() {
+const pascal = function(n) {
+  if (n === 0) return [1];
   
+  const prevRow = pascal(n - 1);
+  const newRow = [1];
+  
+  for (let i = 0; i < prevRow.length - 1; i++) {
+    newRow.push(prevRow[i] + prevRow[i + 1]);
+  }
+  
+  newRow.push(1); 
+  
+  return newRow;
 };
   
 // Do not edit below this line

ad5423b5bbcf6a6e1c90793f45dc331d174fc465
SHOIYAN <shoiyan9@gmail.com>
2026-01-07 22:19:07 +0300

    feat: finish permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,19 @@
-const permutations = function() {
+const permutations = function(arr) {
+  if (arr.length === 0) return [[]];
   
+  const result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+    const permsOfRemaining = permutations(remaining);
+    
+    for (const perm of permsOfRemaining) {
+      result.push([current, ...perm]);
+    }
+  }
+  
+  return result;
 };
   
 // Do not edit below this line
